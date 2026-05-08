3bdb6ad8ccc82332ebacbe264ed91d90bc885cbe
Rafael ID <ralf_raid@outlook.es>
2026-05-08 15:50:47 -0600

    feat (APP):  se inicializa la aplicacion como standalone component

--- inventory-app/src/app/app.component.ts
+++ inventory-app/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'inventory-app';
