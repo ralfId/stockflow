950e0e8c1e93cdd4becce218e160423523d536b1
Rafael ID <ralf_raid@outlook.es>
2026-05-09 08:30:03 -0600

    feat(APP): maquetacion panel de alertas

--- inventory-app/src/app/features/stock-alerts/stock-alerts.component.ts
+++ inventory-app/src/app/features/stock-alerts/stock-alerts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-stock-alerts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './stock-alerts.component.html',
   styleUrls: ['./stock-alerts.component.css']
 })
